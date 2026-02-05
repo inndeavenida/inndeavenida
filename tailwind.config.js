7c3dcb22fe65741fe6e00a44b9883f7b15d309f0
Tony Johnson <tony@tosp.net.au>
2026-02-05 11:23:28 +1100

    Added dev files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        spectral: ["Spectral"],
+        system: ["Arial, Helvetica, sans-serif"],
+      },
+    },
+  },
+};
